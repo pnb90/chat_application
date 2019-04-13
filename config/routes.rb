e8517c35ea5c982adb6f7a94f5bc62f700dd3c2b
pnb90 <phuocbui90@gmail.com>
2019-04-13 12:50:25 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # namespace :api do
+  #   get "/photos" => "photos#index"
+  # end
+end
